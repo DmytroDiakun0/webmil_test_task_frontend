f78d7d7aeb12cc618a32b2475de10238c5764694
DmytroDiakun0 <diakun.dm@gmail.com>
2024-02-13 20:03:18 +0200

    Refactor OverviewContent component with react hooks

--- src/Components/OverviewContent/OverviewContent.js
+++ src/Components/OverviewContent/OverviewContent.js
@@ -1,105 +1,93 @@
 import {Box, Grid} from "@mui/material";
+import overviewContentStyles from "./OverviewContent_styles";
 import CustomerContainer from "../../Store/Containers/CustomerContainer";
 import VehicleContainer from "../../Store/Containers/VehicleContainer";
 import ContractContainer from "../../Store/Containers/ContractContainer";
 import EventsContainer from "../../Store/Containers/EventsContainer";
 import CustomerCardEditContainer from "../../Store/Containers/CustomerCardEditContainer";
-import {Component} from "react";
+import {useState} from "react";
 import VehicleCardEditContainer from "../../Store/Containers/VehicleCardEditContainer";
 import ContractCardEditContainer from "../../Store/Containers/ContractCardEditContainer";
 
-class OverviewContent extends Component {
-    constructor(props) {
-        super(props);
+function OverviewContent() {
+    const classes = overviewContentStyles();
 
-        this.state = {
-            customerMode: 'view',
-            vehicleMode: 'view',
-            contractMode: 'view'
-        }
+    const [customerMode, setCustomerMode] = useState('view');
+    const [vehicleMode, setVehicleMode] = useState('view');
+    const [contractMode, setContractMode] = useState('view');
 
-        this.changeCustomerMode = this.changeCustomerMode.bind(this);
-        this.customerCard = this.customerCard.bind(this);
-        this.changeVehicleMode = this.changeVehicleMode.bind(this);
-        this.vehicleCard = this.vehicleCard.bind(this);
-        this.changeContractMode = this.changeContractMode.bind(this);
-        this.contractCard = this.contractCard.bind(this);
-    }
-
-    changeCustomerMode() {
-        if(this.state.customerMode === 'view') {
-            this.setState({ customerMode : 'edit'})
+    const changeCustomerMode = () => {
+        if(customerMode === 'view') {
+            setCustomerMode('edit')
         } else {
-            this.setState({ customerMode : 'view'})
+            setCustomerMode('view')
         }
     }
 
-    changeVehicleMode() {
-        if(this.state.vehicleMode === 'view') {
-            this.setState({ vehicleMode : 'edit'})
+    const changeVehicleMode = () => {
+        if(vehicleMode === 'view') {
+            setVehicleMode('edit')
         } else {
-            this.setState({ vehicleMode : 'view'})
+            setVehicleMode('view')
         }
     }
 
-    changeContractMode() {
-        if(this.state.contractMode === 'view') {
-            this.setState({ contractMode : 'edit'})
+    const changeContractMode = () => {
+        if(contractMode === 'view') {
+            setContractMode('edit')
         } else {
-            this.setState({ contractMode : 'view'})
+            setContractMode('view')
         }
     }
 
-    customerCard() {
-        if(this.state.customerMode === 'edit') {
-            return (<CustomerCardEditContainer changeCardFunc={this.changeCustomerMode}/>)
-        } else if(this.state.customerMode === 'view') {
-            return (<CustomerContainer changeCardFunc={this.changeCustomerMode}/>)
+    const customerCard = () => {
+        if(customerMode === 'edit') {
+            return (<CustomerCardEditContainer changeCardFunc={changeCustomerMode}/>)
+        } else if(customerMode === 'view') {
+            return (<CustomerContainer changeCardFunc={changeCustomerMode}/>)
         }
     }
 
-    vehicleCard() {
-        if(this.state.vehicleMode === 'edit') {
-            return (<VehicleCardEditContainer changeCardFunc={this.changeVehicleMode}/>)
-        } else if(this.state.vehicleMode === 'view') {
-            return (<VehicleContainer changeCardFunc={this.changeVehicleMode}/>)
+    const vehicleCard = () => {
+        if(vehicleMode === 'edit') {
+            return (<VehicleCardEditContainer changeCardFunc={changeVehicleMode}/>)
+        } else if(vehicleMode === 'view') {
+            return (<VehicleContainer changeCardFunc={changeVehicleMode}/>)
         }
     }
 
-    contractCard() {
-        if(this.state.contractMode === 'edit') {
-            return (<ContractCardEditContainer changeCardFunc={this.changeContractMode}/>)
-        } else if(this.state.contractMode === 'view') {
-            return (<ContractContainer changeCardFunc={this.changeContractMode}/>)
+    const contractCard = () => {
+        if(contractMode === 'edit') {
+            return (<ContractCardEditContainer changeCardFunc={changeContractMode}/>)
+        } else if(contractMode === 'view') {
+            return (<ContractContainer changeCardFunc={changeContractMode}/>)
         }
     }
 
-    render() {
-        return (
-            <Grid container className={this.props.classes.overviewContent}>
-                <Grid item xs={6}>
-                    <Box className={this.props.classes.gridItemContent}>
-                        {this.customerCard()}
-                    </Box>
-                </Grid>
-                <Grid item xs={6}>
-                    <Box className={this.props.classes.gridItemContent}>
-                        {this.vehicleCard()}
-                    </Box>
-                </Grid>
-                <Grid item xs={12}>
-                    <Box className={this.props.classes.gridItemContent}>
-                        {this.contractCard()}
-                    </Box>
-                </Grid>
-                <Grid item xs={12}>
-                    <Box className={this.props.classes.gridItemContent}>
-                        <EventsContainer/>
-                    </Box>
-                </Grid>
+    return (
+        <Grid container className={classes.overviewContent}>
+            <Grid item xs={6}>
+                <Box className={classes.gridItemContent}>
+                    {customerCard()}
+                </Box>
             </Grid>
-        );
-    }
+            <Grid item xs={6}>
+                <Box className={classes.gridItemContent}>
+                    {vehicleCard()}
+                </Box>
+            </Grid>
+            <Grid item xs={12}>
+                <Box className={classes.gridItemContent}>
+                    {contractCard()}
+                </Box>
+            </Grid>
+            <Grid item xs={12}>
+                <Box className={classes.gridItemContent}>
+                    <EventsContainer/>
+                </Box>
+            </Grid>
+        </Grid>
+    );
 }
 
 export default OverviewContent;
